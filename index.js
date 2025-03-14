13ad6fba23ec6a9e81cf1b894a420457759883d3
kimberly <maldakimberly228@gmail.com>
2025-03-14 23:38:19 +0300

    create a file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function() {
+console.log("The DOM has loaded");
+});
+console.log(
+    "Before DOMContentLoaded"
+        
+)
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('p')
+    .textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically.");
+    document.querySelector('p')
+    .textContent = "This is really cool!";
+});
